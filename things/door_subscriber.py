f5e8b12aa7ee276cfd9516c2622029ab3ce4b1dd
dindasigma <dinda.sgmwty@gmail.com>
2019-10-28 23:12:18 +0700

    remove extra spaces

--- things/door_subscriber.py
+++ things/door_subscriber.py
@@ -1,13 +1,13 @@
 import yaml
-from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import json
 from time import sleep
+import datetime
 import RPi.GPIO as GPIO # import Raspberry Pi GPIO library
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 import logging
 import boto3
-import json
 import uuid
-import time
-import datetime
+
 
 GPIO.setwarnings(True) # ignore warning
 GPIO.setmode(GPIO.BOARD) # use physical pin numbering
@@ -22,60 +22,60 @@ table_log = dynamodb.Table(config['table_log'])
 
 # callback from AWS subscribe
 def doors_callback(client, userdata, message):
-	print("\nReceived message: ")
-	print(message.payload)
-	print("From topic: ")
-	print(message.topic)
-	thing_switch(message)
-	print("-----------------------------------\n")
+    print("\nReceived message: ")
+    print(message.payload)
+    print("From topic: ")
+    print(message.topic)
+    thing_switch(message)
+    print("-----------------------------------\n")
 
 def create_log(table_log, data):
-	try:
-		time = datetime.datetime.now().strftime('%A, %d. %B %Y %I:%M%p')
+    try:
+        time = datetime.datetime.now().strftime('%A, %d. %B %Y %I:%M%p')
 
-		response_insert = table_log.put_item(
-			Item={
-				'id': str(uuid.uuid4()),
-				'userid': data['requester'],
-				'doorid': data['pin'],
-				'activity': 'open the door',
-				'createdAt': time,
-			}
-		)
-	except ClientError as e:
-				return (e.response['Error']['Message'])
-	else:
-		return ("Recorded")
+        response_insert = table_log.put_item(
+            Item={
+                'id': str(uuid.uuid4()),
+                'userid': data['requester'],
+                'doorid': data['pin'],
+                'activity': 'open the door',
+                'createdAt': time,
+            }
+        )
+    except ClientError as e:
+        return (e.response['Error']['Message'])
+    else:
+        return ("Recorded")
 
 
 def thing_switch(message):
-	data = json.loads(message.payload)
+    data = json.loads(message.payload)
 
-	# GPIO pin initialization
-	pin = int(data["pin"])
-	print(data["pin"])
+    # GPIO pin initialization
+    pin = int(data["pin"])
+    print(data["pin"])
 
-	if data and 'command' in data:
+    if data and 'command' in data:
 
-		# turn on the relay if command is "open"
-		if data["command"] == "open":
-			GPIO.output(pin, GPIO.HIGH)
-			sleep(5)
-			print("After sleep ...")
-			GPIO.output(pin, GPIO.LOW)
-			print("[INFO] The door is open!")
+        # turn on the relay if command is "open"
+        if data["command"] == "open":
+            GPIO.output(pin, GPIO.HIGH)
+            sleep(5)
+            print("After sleep ...")
+            GPIO.output(pin, GPIO.LOW)
+            print("[INFO] The door is open!")
 
-			# create log
-			create_log(table_log, data)
-		else:
-			print("[ERRO] Unknown command!")
+            # create log
+            create_log(table_log, data)
+        else:
+            print("[ERRO] Unknown command!")
 
 # activating gpio pin
 response_scan = table_doors.scan()
 for i in response_scan["Items"]:
-	GPIO.setup(int(i["pin"]), GPIO.OUT)
-	GPIO.output(int(i["pin"]), GPIO.LOW)
-	print("Activating pin: " + str(i["pin"]))
+    GPIO.setup(int(i["pin"]), GPIO.OUT)
+    GPIO.output(int(i["pin"]), GPIO.LOW)
+    print("Activating pin: " + str(i["pin"]))
 
 # configure loggger
 logger = logging.getLogger("AWSIoTPythonSDK.core")
@@ -96,19 +96,19 @@ thing.configureConnectDisconnectTimeout(10)
 thing.configureMQTTOperationTimeout(5)
 
 try:
-	thing.connect()
-	thing.subscribe(config['topic'], 1, doors_callback)
+    thing.connect()
+    thing.subscribe(config['topic'], 1, doors_callback)
 
-	while True:
-		 """ I'm loop """
+    while True:
+         """ I'm loop """
 
 except KeyboardInterrupt:
-	print("Cleaning up GPIO")
-	GPIO.cleanup()
-	thing.unsubscribe(config['topic'])
+    print("Cleaning up GPIO")
+    GPIO.cleanup()
+    thing.unsubscribe(config['topic'])
 
 finally:
-	print("Cleaning up GPIO")
-	GPIO.cleanup()
-	thing.unsubscribe(config['topic'])
+    print("Cleaning up GPIO")
+    GPIO.cleanup()
+    thing.unsubscribe(config['topic'])
 
